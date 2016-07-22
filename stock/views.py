ff586a6e3b60c5779c3826ce4996197f73962891
Rafael F.V.C. Santos <rafaelfvcs@gmail.com>
2016-07-22 12:34:18 -0300

    add views and templates

--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render
+from .models import Stock
 
-def index(request):
-    return HttpResponse('<h1>This is my second homepage STOCK</h1>')
+
+def index(request): # oareu de atualizar 13 - 1:40 min
+    all_stocks = Stock.objects.all()
+    context = {'all_stocks': all_stocks}
+    return render(request, 'stock/index.html', context)
+
+def detail(request, stock_id):
+    try:
+        stock = Stock.objects.get(pk=stock_id)
+    except Stock.DoesNotExist:
+        raise Http404('Stock does not exist!')
+    return render(request, 'stock/detail.html', {'stock': stock})
