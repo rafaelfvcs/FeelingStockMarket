8208a6913676e555dfb547bb4c466ce4af940d48
Rafael F.V.C. Santos <rafaelfvcs@gmail.com>
2016-08-02 18:52:37 -0300

    Add index navbar

--- stock/urls.py
+++ stock/urls.py
@@ -8,11 +8,16 @@ Created on Thu Jul 21 16:26:41 2016
 from django.conf.urls import url
 from . import views
 
+app_name = 'stock'
+
 urlpatterns = [
     # /stock/
     url(r'^$', views.index, name='index'),
 
     # /stock/stock.id/
     url(r'^(?P<stock_id>[0-9]+)/$', views.detail, name='detail'),
+
+    # /stock/stock.id/analysis/
+    url(r'^(?P<stock_id>[0-9]+)/analysis/$', views.analysis, name='analysis'),
 ]
 
